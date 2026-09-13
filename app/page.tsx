3c96aebaa185e15c826b2d42be16a980bb455087
BlueDragon33 <dinhnam331007@gmail.com>
2026-09-13 18:20:08 +0700

    test(ui): keep ApplicationHub root contract name

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { requireChatGPTUser } from "./chatgpt-auth";
-import ManagementDashboard from "./management-dashboard";
+import ApplicationHub from "./management-dashboard";
 import LocalQuickAccess from "./local-quick-access";
 
 export const dynamic = "force-dynamic";
@@ -7,7 +7,7 @@ export const dynamic = "force-dynamic";
 export default async function Home() {
   const user = await requireChatGPTUser("/");
   return <>
-    <ManagementDashboard user={{ displayName: user.displayName, email: user.email }} />
+    <ApplicationHub user={{ displayName: user.displayName, email: user.email }} />
     <LocalQuickAccess />
   </>;
 }
