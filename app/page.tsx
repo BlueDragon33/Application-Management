4864691acf3207cbe2092b88aacd79fc4f285bb8
BlueDragon33 <dinhnam331007@gmail.com>
2026-09-17 14:00:51 +0700

    feat: enable approved persistent dashboard layout

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import "./management-dashboard-reference-layout.css";
 import "./management-quick-actions.css";
 import "./management-modern-overview.css";
 import "./management-latest-layout.css";
+import "./management-dashboard-image-layout.css";
 import LocalQuickAccess from "./local-quick-access";
 
 export const dynamic = "force-dynamic";
