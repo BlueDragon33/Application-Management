33c4446aab20e669a977b178b9c18426ef8e7259
BlueDragon33 <dinhnam331007@gmail.com>
2026-09-17 14:14:57 +0700

    style: load approved dashboard v2 skin

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import "./management-quick-actions.css";
 import "./management-modern-overview.css";
 import "./management-latest-layout.css";
 import "./management-dashboard-image-layout.css";
+import "./management-dashboard-v2.css";
 import LocalQuickAccess from "./local-quick-access";
 
 export const dynamic = "force-dynamic";
